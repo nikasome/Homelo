d4878a3f6e6e3b33e44f57a5ee1e75fea5694181
nikasome <kou1pomu@yahoo.co.jp>
2019-03-27 18:42:44 +0900

    add JSON read & link

--- src/App.js
+++ src/App.js
@@ -2,27 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+const HOMETE_LIST = [
+  { homete_id: 'AAAA', title: 'ラーメン完飲' },
+  { homete_id: 'BBBB', title: '生きた' },
+  { homete_id: 'CCCC', title: '自炊した' },
+  { homete_id: 'DDDD', title: '100点とった' }
+];
+
 const App = () => (
   <BrowserRouter>
     <div>
-      <Route exact path="/home" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/homete_post" component={Post} />
       <Route path="/homete/:id" component={Homete} />
     </div>
   </BrowserRouter>
 );
 
-const Home = () => (
-  <div>
-    <h1>メイン画面</h1>
-  </div>
-);
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>メイン画面</h1>
+        <ul>
+          <li>
+            <Link to="/homete_post">投稿する</Link>
+          </li>
+          <li>
+            <Link to="/homete/123">見る</Link>
+          </li>
+        </ul>
+        <ul>
+          {HOMETE_LIST.map(function(homete, i) {
+            return (
+              <div key={i}>
+                <Link to={'/homete/' + homete.homete_id}>{homete.title}</Link>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
-const Post = () => (
-  <div>
-    <h1>Homete投稿画面</h1>
-  </div>
-);
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homete投稿画面</h1>
+      </div>
+    );
+  }
+}
 
 class Homete extends Component {
   render() {

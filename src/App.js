bb684084d34ab1581f8f53a44ec49b090d0d3f59
nikasome <kou1pomu@yahoo.co.jp>
2019-03-27 17:55:09 +0900

    add screen transfer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-class App extends Component {
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/home" component={Home} />
+      <Route path="/homete_post" component={Post} />
+      <Route path="/homete/:id" component={Homete} />
+    </div>
+  </BrowserRouter>
+);
+
+const Home = () => (
+  <div>
+    <h1>メイン画面</h1>
+  </div>
+);
+
+const Post = () => (
+  <div>
+    <h1>Homete投稿画面</h1>
+  </div>
+);
+
+class Homete extends Component {
   render() {
+    let { id } = this.props.match.params;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{id}</h1>
       </div>
     );
   }
